6298df9a1d7ae76bde78fca351b02a54b31d194e
Tom Hinkle <tmhinkle@gmail.com>
2019-05-30 08:49:42 -0400

    Simple resizing library

---
+++ resize.js
@@ -0,0 +1,46 @@
+/* A simple resize library */
+const sharp = require('sharp');
+const glob = require('glob');
+
+function addSuffix (fn, suffix) {
+    filenames = fn.split('.')
+    filenames.splice(filenames.length-1,0,suffix)
+    return filenames.join('.');
+}
+
+function resize (fn, s) {
+    outName = addSuffix(fn,''+s);
+    sharp(fn)   
+    .resize({
+        width : s,
+        height : s,
+        fit : 'inside',
+        withoutEnlargement : true,
+    })
+        .toFile(outName,
+                function (err) {
+                    if (err) {console.log('Error writing %s: %s',outName,err)};
+                }
+               )
+}
+
+var resizer = {
+    resize : resize,
+    resizeAll : function (fn) {
+        this.sizes.map((s)=>this.resize(fn,s));
+    },
+    crawl : function ({
+        files=[],
+        dir='images',
+        extensions=['jpg','jpeg','png','gif','webm']
+    }) {
+        extensions.forEach(
+            (ext)=>{files.push(...glob.sync(`${dir}/*.${ext}`))}
+        ); 
+        files.forEach((fn)=>this.resizeAll(fn)) // resize files...
+        var dirs = glob.sync(`./${dir}/*/`);
+        dirs.forEach((d)=>this.crawl({dir:d,files,extensions})); // recurse...
+    },
+    sizes : [1920,800,400,150],
+}
+exports.resizer = resizer;
