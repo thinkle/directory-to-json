9f9fd1efa8c686c08c2e4dd8b7fdc96310b2bf79
Tom Hinkle <tmhinkle@gmail.com>
2019-09-09 22:29:39 -0400

    Add filter call to avoid resized

--- build.js
+++ build.js
@@ -7,6 +7,7 @@ directoryToJson.buildDirectory(
         outputFile:'galleries.js', // Create a galleries.js file to contain our galleries data
         jsonPrefix:'var galleries=', // Store JSON output in "galleries" global variable :)
         outputTransform: (fn)=>resizer.resizer.mapSizes(fn), // make each image a map of different available filesizes
+        filter : (fn)=>!fn.match(resizer.resizer.resizedMatcher),
     });
 
 
